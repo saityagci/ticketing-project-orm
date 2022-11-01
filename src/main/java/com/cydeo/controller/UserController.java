c1a2344bf2c0a87f896d891641db7f9331ea56aa
saityagci <saityagci@gmail.com>
2022-10-31 20:49:54 -0400

    part 1

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,24 +13,24 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-//
-//    RoleService roleService;
-//    UserService userService;
-//
-//    public UserController(RoleService roleService, UserService userService) {
-//        this.roleService = roleService;
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/create")
-//    public String createUser(Model model) {
-//
-//        model.addAttribute("user", new UserDTO());
-//        model.addAttribute("roles", roleService.findAll());
-//        model.addAttribute("users", userService.findAll());
-//
-//        return "/user/create";
-//    }
+
+    private  final RoleService roleService;
+    private final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.listAllRoles());
+        model.addAttribute("users", userService.listAllUsers());
+
+        return "/user/create";
+    }
 //
 //
 //    @PostMapping("/create")
---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<UserDTO> listAllUsers() {
+        return null;
+    }
+
+    @Override
+    public UserDTO findByUserName(String name) {
+        return null;
+    }
+
+    @Override
+    public void save(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public UserDTO update(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteByUserName(String username) {
+
+    }
+}

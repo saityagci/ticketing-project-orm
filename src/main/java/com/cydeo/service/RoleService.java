cf6bfb139cb002cc65e675ef4ae2e2f2bef86e4a
saityagci <saityagci@gmail.com>
2022-10-31 20:26:11 -0400

    part 1

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,6 +1,4 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.RoleDTO;
-
-public interface RoleService extends CrudService<RoleDTO, Long> {
+public interface RoleService {
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,13 +1,8 @@
 package com.cydeo.service;
 
-import com.cydeo.dto.UserDTO;
 
-import java.util.List;
 
-public interface UserService extends CrudService<UserDTO, String> {
+public interface UserService {
 
-    List<UserDTO> findManagers();
-
-    List<UserDTO> findEmployees();
 
 }

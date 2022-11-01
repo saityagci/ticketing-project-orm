dee75f544e56abdeb98dc6ec3cb05e10ee57c422
saityagci <saityagci@gmail.com>
2022-10-31 20:10:00 -0400

    part 1

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Role {
-    private Long id;
+@Table(name = "roles")
+@Entity
+public class Role extends BaseEntity{
     private String description;
 
 }

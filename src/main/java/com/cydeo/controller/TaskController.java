6a961be4af15282c5f86a7d4fba0f61a799864f4
saityagci <saityagci@gmail.com>
2022-11-14 21:47:47 -0500

    part 4

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -72,30 +72,30 @@ public class TaskController {
         return "task/update";
 
     }
-//
-////    @PostMapping("/update/{taskId}")
-////    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
-////        task.setId(taskId);
-////        taskService.update(task);
-////        return "redirect:/task/create";
-////    }
-//
-//    @PostMapping("/update/{id}")
-//    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
-//
-//        if (bindingResult.hasErrors()) {
-//
-//            model.addAttribute("projects", projectService.findAll());
-//            model.addAttribute("employees", userService.findEmployees());
-//            model.addAttribute("tasks", taskService.findAll());
-//
-//            return "/task/update";
-//
-//        }
-//
+
+//    @PostMapping("/update/{taskId}")
+//    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
+//        task.setId(taskId);
 //        taskService.update(task);
 //        return "redirect:/task/create";
 //    }
+
+    @PostMapping("/update/{id}")
+    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.listAllProject());
+            model.addAttribute("employees", userService.listAllByRole("employee"));
+            model.addAttribute("tasks", taskService.listAllTasks());
+
+            return "/task/update";
+
+        }
+
+        taskService.update(task);
+        return "redirect:/task/create";
+    }
 //
 //    @GetMapping("/employee/pending-tasks")
 //    public String employeePendingTasks(Model model) {
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -50,6 +50,14 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void update(TaskDTO dto) {
+        Optional<Task> task=taskRepository.findById(dto.getId());
+        Task convertedTask=taskMapper.convertToEntity(dto);
+        if (task.isPresent()){
+            convertedTask.setId(task.get().getId());
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
 
     }
 
